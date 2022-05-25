1ff1cd58059ef457d3e77392ea8d24679f4e4d0d
Denibly <106178943+Denibly@users.noreply.github.com>
2022-05-25 18:15:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281278612831','6281278612831','62811278612831'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281278612831'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281278612831' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Dennz_Botz' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Dennz-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Dennz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Dennz_Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Dennz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
